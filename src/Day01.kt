7a472cea197942c9626b82ec903833a734acdb99
Jan Meier <jan@meier.wtf>
2021-12-19 10:11:31 +0100

    Solves Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
+import java.lang.Integer.max
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>): Int {
+		val inputAsIntArray = input.map { it.toInt() }.toIntArray()
+		var counter = 0
+		inputAsIntArray
+			.forEachIndexed { index, value ->
+				if (value > inputAsIntArray[max(index - 1, 0)]) {
+					counter++
+				}
+			}
+		return counter
+	}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	fun part2(input: List<String>): Int {
+		val result = input.map { it.toInt() }
+			.windowed(3,1, false)
+			.map { it.sum() }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+		return part1(result.map { it.toString() })
+	}
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+	// test if implementation meets criteria from the description, like:
+	val testInput = readInput("Day01_test")
+	check(part1(testInput) == 7)
+	check(part2(testInput) == 5)
+
+	val input = readInput("Day01")
+	println(part1(input))
+	println(part2(input))
 }
